469d4501354cfcab3c7f262536ab54270a133d7c
Gatsby01k <vladyslavddos@gmail.com>
2025-11-25 15:35:17 +0300

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,8 +1,5 @@
-"use client";
-
-import { useEffect } from "react";
 import Hero from "./components/Hero";
-import MetricsStrip from "./components/MetricsStrip"; // <-- ВАЖНО
+import MetricsStrip from "./components/MetricsStrip";
 import WhyNow from "./components/WhyNow";
 import Pillars from "./components/Pillars";
 import Architecture from "./components/Architecture";
@@ -10,37 +7,22 @@ import Stability from "./components/Stability";
 import Roadmap from "./components/Roadmap";
 import Team from "./components/Team";
 import Investors from "./components/Investors";
+import RevealClient from "./components/RevealClient";
 
 export default function Page() {
-  useEffect(() => {
-    const observer = new IntersectionObserver(
-      (entries) => {
-        entries.forEach((entry) => {
-          if (entry.isIntersecting) {
-            entry.target.classList.add("visible");
-            observer.unobserve(entry.target);
-          }
-        });
-      },
-      { threshold: 0.12 }
-    );
-
-    document.querySelectorAll(".reveal").forEach((el) => observer.observe(el));
-
-    return () => observer.disconnect();
-  }, []);
-
   return (
-    <>
-      <Hero />
-      <MetricsStrip /> {/* <-- ВОТ СЮДА! */}
-      <WhyNow />
-      <Pillars />
-      <Architecture />
-      <Stability />
-      <Roadmap />
-      <Team />
-      <Investors />
-    </>
+    <RevealClient>
+      <>
+        <Hero />
+        <MetricsStrip />
+        <WhyNow />
+        <Pillars />
+        <Architecture />
+        <Stability />
+        <Roadmap />
+        <Team />
+        <Investors />
+      </>
+    </RevealClient>
   );
 }
